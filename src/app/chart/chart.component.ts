5327caa2457991bf3bfa561173f4021b8c47188f
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2020-01-23 21:33:59 -0600

    at least works in both directions but still doesn't scale up

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -4,13 +4,16 @@ import {
   ViewChild,
   HostListener,
   Component,
-  ElementRef
+  ElementRef,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
 } from "@angular/core";
 
 @Component({
   selector: "app-chart",
   templateUrl: "./chart.component.html",
-  styleUrls: ["./chart.component.css"]
+  styleUrls: ["./chart.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChartComponent implements OnChanges, AfterViewInit {
   private yScale: number = 100;
@@ -19,10 +22,11 @@ export class ChartComponent implements OnChanges, AfterViewInit {
   );
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
-  private width: number = 100;
+  private width: number;
   private height: number;
   private barWidth: number;
-  constructor(private elRef: ElementRef) {}
+
+  constructor(private elRef: ElementRef, private cdr: ChangeDetectorRef) {}
 
   @ViewChild("myCanvas", { static: false }) set canvasRef(
     canvasRef: ElementRef
@@ -40,7 +44,12 @@ export class ChartComponent implements OnChanges, AfterViewInit {
     //Recalculate derived values
     this.barWidth = this.width / this.data.length;
     //this.yScale = .... very likely the max range of values ....
+
+    this.cdr.detectChanges();
+
     this.draw();
+
+    this.cdr.detectChanges();
   }
 
   // When the window is resized, recalculate our canvas dimensions and redraw.
@@ -48,23 +57,34 @@ export class ChartComponent implements OnChanges, AfterViewInit {
   onResize() {
     // The canvas will fill the width and height of the host element.
     // See what they are and set them for the canvas.
-    this.width = this.elRef.nativeElement.offsetWidth;
     this.height = this.elRef.nativeElement.offsetHeight;
+    this.width = this.elRef.nativeElement.offsetWidth;
+
+    this.cdr.detectChanges();
+
     // We're manually setting these instead of using property bindings in
     // the view, because we need these to be set before we invoke "draw".
     // HTML5 canvas is always cleared when its width/height is set, so
     // the order in which this happens is significant.
+
     this.canvas.width = this.width;
+    // this.canvas.style.width = `${this.width}px`;
+
     this.canvas.height = this.height;
-    this.canvas.style.width = `${this.width}px`;
-    this.canvas.style.height = `${this.height}px`;
+    //this.canvas.style.height = `${this.height}px`;
 
     //Recalculate derived values
     this.barWidth = this.width / this.data.length;
+
+    this.cdr.detectChanges();
+
     this.draw();
+
+    this.cdr.detectChanges();
   }
 
   private draw() {
+    this.cdr.detectChanges();
     this.ctx.translate(0, this.height);
     this.ctx.scale(1, -1);
     this.ctx.clearRect(0, 0, this.width, this.height);
@@ -77,5 +97,6 @@ export class ChartComponent implements OnChanges, AfterViewInit {
         (value / this.yScale) * this.height
       );
     });
+    this.cdr.detectChanges();
   }
 }
